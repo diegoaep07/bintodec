df698b42d2ef55a6a822a70342b7999e1c1c1142
Diego <diegoaep07@proton.me>
2025-12-22 11:28:52 -0500

    Funcion BinToDec recreada y optimizada

--- bintodec.js
+++ bintodec.js
@@ -1,71 +1,58 @@
 //Funcion para llevar el binario bincode a decimal
-function binToDec(bincode){
+const binToDec = (bincode) => {
+  let reversedBinCodeArr = bincode.split("").reverse();
+  let dec = 0;
 
-	let revBinCodeArr = bincode.split('').reverse()
-	let dec=0
+  for (let i = 0; i < reversedBinCodeArr.length; i++) {
+    if (reversedBinCodeArr[i] === "0") {
+      dec += 0;
+    } else {
+      dec += 2 ** i;
+    }
+  }
 
-	for(let i=0; i<revBinCodeArr.length; i++){
-		if(revBinCodeArr[i] === '0'){
-			dec+=0
-		}
-		else if(revBinCodeArr[i] === '1'){
-			if(i===0){
-				dec+=1
-			}
-			else{
-				dec += Number(revBinCodeArr[i])*(twoPot(i))
-			}
-		}
-		else{
-			return 'El codigo no es un Binario'
-		}
-	}
-
-
-	return dec
-}
-
+  return dec;
+};
 
 //Funcion para llevar el decimal decCode a Binario
-function decToBin(decCode){
-	let binary = ''
-	let div = decCode
+function decToBin(decCode) {
+  let binary = "";
+  let div = decCode;
 
-	for(let i=0;div > 0;i++){
-		if(div%2 === 0){
-			binary += '0'
-			div = div/2
-		}
-		else{
-			binary += '1'
-			div = Math.floor(div/2)
-		}
-	}
+  for (let i = 0; div > 0; i++) {
+    if (div % 2 === 0) {
+      binary += "0";
+      div = div / 2;
+    } else {
+      binary += "1";
+      div = Math.floor(div / 2);
+    }
+  }
 
-	binary = reverse(binary)
+  binary = reverse(binary);
 
-	return binary
+  return binary;
 }
 
-function reverse(strings){
-	let str = strings.split('').reverse()
-	let revstr=''
+function reverse(strings) {
+  let str = strings.split("").reverse();
+  let revstr = "";
 
-	for(let i=0; i<str.length; i++){
-		revstr+=str[i]
-	}
-	return revstr
+  for (let i = 0; i < str.length; i++) {
+    revstr += str[i];
+  }
+  return revstr;
 }
-
 
 //Funcion para elevar 2 al exponente de int
-function twoPot(int){
-	let potTwo = 2
-	for(let i=2; i<=int; i++){
-		potTwo*=2
-	}
-	return potTwo
+function twoPot(int) {
+  let potTwo = 2;
+  for (let i = 2; i <= int; i++) {
+    potTwo *= 2;
+  }
+  return potTwo;
 }
 
-exports.binToDec = binToDec
-exports.decToBin = decToBin
+exports.binToDec = binToDec;
+exports.decToBin = decToBin;
+
