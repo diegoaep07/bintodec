cfbee3ace2073ff1767b2ab8036a784a152dfb46
Diego <diegoaep07@proton.me>
2026-01-16 10:37:44 -0500

    Big Binaty Number Test

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 let bintodec = require('./bintodec.js')
 
-console.log(bintodec.binToDec('10110'))
+console.log(bintodec.binToDec('11110000100111111001100010000010'))
 console.log(bintodec.decToBin(22))
